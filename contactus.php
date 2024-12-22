1d6af293e8daaac8f375924cac80a7f39980eb38
LeDuyAnhTu <22211TT0024@mail.tdc.edu.vn>
2024-12-22 10:34:23 +0700

    trang liên hệ

--- contactus.php
+++ contactus.php
@@ -23,38 +23,73 @@ Modified: 19:51 21/12/2024
     <section id="contactus">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 col-lg-6">
-                    <h1 class="contactus-title">
-                        Thông tin liên hệ
-                    </h1>
-                    <p>Hãy liên lạc với các chuyên gia của XE2GO để nhận dịch vụ chất lượng!</p>
-                    <ul>
-                        <ul class="list-unstyled">
-                            <li class="mb-3">
-                                <i class="fas fa-phone fa-2x"></i>
-                                <span class="ms-3">0348.798.398 - <a href="#" class="text-decoration-none">Liên hệ Zalo</a></span>
-                            </li>
-                            <li class="mb-3">
-                                <i class="fas fa-envelope fa-2x"></i>
-                                <span class="ms-3">xe2govn@gmail.com</span>
-                            </li>
-                            <li class="mb-3">
-                                <i class="fas fa-map-marker-alt fa-2x"></i>
-                                <span class="ms-3">559 Phạm Ngọc Thạch, Phường Phú Mỹ, TP Thủ Dầu Một, Bình Dương</span>
-                            </li>
-                            <li class="mb-3">
-                                <i class="fas fa-clock fa-2x"></i>
-                                <span class="ms-3">Thứ 2 - Thứ 7 | 08:00 - 17:00</span>
-                            </li>
-                        </ul>
-                    </ul>
+                <div class="col-md-6 col-lg-6 col-sm-12">
+                    <div class="col-inner">
+                        <h2 class="contactus-title">
+                            Thông tin liên hệ
+                        </h2>
+                        <hr>
+                        <p>Hãy liên lạc với các chuyên gia của XE2GO để nhận dịch vụ chất lượng!</p>
+                        <!-- Liên hệ -->
+                        <div class="icon-box">
+                            <div class="icon-box-img" style="width:50px">
+                                <div class="icon-inner" style="color:rgb(4, 37, 42);">
+                                    <img src="./public/svg/lienhe.svg" alt="Liên hệ">
+                                </div>
+                            </div>
+                            <div class="icon-box-text">
+                                <h4>HOTLINE & ZALO</h4>
+                                <p>
+                                    <a href="tel:0348798398">0348.798.398</a>
+                                    -
+                                    <a href="https://zalo.me/3437214821843033113" target="_blank" rel="noopener">Liên hệ Zalo</a>
+                                </p>
+                            </div>
+                        </div>
+
+                        <!-- Email -->
+                        <div class="icon-box">
+                            <div class="icon-box-img" style="width:50px">
+                                <div class="icon-inner" style="color:rgb(4, 37, 42);">
+                                    <img src="./public/svg/email.svg" alt="Liên hệ" class="img-fluid">
+                                </div>
+                            </div>
+                            <div class="icon-box-text">
+                                <h4>EMAIL</h4>
+                                <p><a href="mailto:xe2govn@gmail.com" target="_blank" rel="noopener">xe2govn@gmail.com</a></p>
+                            </div>
+                        </div>
+
+                        <!-- Địa chỉ -->
+                        <div class="icon-box">
+                            <div class="icon-box-img" style="width:50px">
+                                <div class="icon-inner" style="color:rgb(4, 37, 42);">
+                                    <img src="./public/svg/diachi.svg" alt="Dịa chỉ" class="img-fluid">
+                                </div>
+                            </div>
+                            <div class="icon-box-text">
+                                <h4>ĐỊA CHỈ</h4>
+                                <p>559 Phạm Ngọc Thạch, Phường Phú Mỹ, Tp Thủ Dầu Một, Bình Dương</p>
+                            </div>
+                        </div>
+
+                        <!-- Giờ làm việc -->
+                        <div class="icon-box">
+                            <div class="icon-box-img" style="width:50px">
+                                <div class="icon-inner" style="color:rgb(4, 37, 42);">
+                                    <img src="./public/svg/giolamviec.svg" alt="Giờ làm việc" class="img-fluid">
+                                </div>
+                            </div>
+                            <div class="icon-box-text">
+                                <h4>GIỜ LÀM VIỆC</h4>
+                                <p>Thứ 2 – Thứ 7 | 08:00 – 17:00</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
                 <div class="col-md-6 col-lg-6">
-                    <div id="my_map_add" style="width:100%;height:300px;"></div>
-
-                    <!-- <div class="map">
-                    <iframe src="https://www.google.com/maps/embed" frameborder="0" style="width:100%; height:300px;" allowfullscreen></iframe>
-                </div> -->
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.37876293776!2d106.67321167504524!3d11.01018248915315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d113324fcbc5%3A0xd21351ea9ecae634!2sXE2GO!5e0!3m2!1svi!2s!4v1726670426565!5m2!1svi!2s" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
@@ -74,25 +109,5 @@ Modified: 19:51 21/12/2024
     <?php include("../xe2go/public/templates/footer.php") ?>
     <?php include("../xe2go/public/templates/script.php") ?>
 </body>
-
-<script type="text/javascript">
-    function my_map_add() {
-        var myMapCenter = new google.maps.LatLng(11.011506084026417, 106.67267845123271);
-        var myMapProp = {
-            center: myMapCenter,
-            zoom: 12,
-            scrollwheel: false,
-            draggable: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("my_map_add"), myMapProp);
-        var marker = new google.maps.Marker({
-            position: myMapCenter
-        });
-        marker.setMap(map);
-    }
-</script>
-
-<script src="https://maps.googleapis.com/maps/api/js?key=your_key&callback=my_map_add"></script>
 
 </html>
